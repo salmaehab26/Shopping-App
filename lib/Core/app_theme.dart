1ec958930bb7d4fb9fa1b54350c7e7123f1dfa7a
salmaehab26 <zalmaehab2002@gmail.com>
2025-03-03 12:24:20 +0200

    first commit

---
+++ lib/Core/app_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'my_colors.dart';
+
+class AppTheme {
+  static ThemeData mainTheme = ThemeData(
+      primaryColor: AppColors.primaryColor,
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.w600,
+          color: AppColors.whiteColor,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w400,
+          color: AppColors.whiteColor,
+        ),
+      ));
+}
---
+++ lib/Features/Splash_Screen.dart
@@ -0,0 +1,69 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+@override
+_SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // إنشاء الأنيميشن
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: -0.1, end: 0.1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+
+    // الانتقال إلى Home Page بعد 3 ثوانٍ
+    // Future.delayed(Duration(seconds: 10), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => HomePage()),
+    //   );
+    // }
+    // );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0XFF9775FA),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: AnimatedBuilder(
+              animation: _animation,
+              builder: (context, child) {
+                return  Transform.rotate(
+                  angle: _animation.value, // تدوير الصورة بشكل طفيف
+                  alignment: Alignment.topCenter, // تثبيت الجزء العلوي وتحريك السفلي
+                  child: Image.asset('assets/Splash.png', width: 150),
+                );
+              },
+            ),
+          ),
+          Center(child: Column(children: [Text("STYLISH",style:TextStyle(fontSize: 20) ),Text("Find Your Slyle",style:TextStyle(fontSize: 20))]),)
+        ],
+      ),
+    );
+  }
+}
